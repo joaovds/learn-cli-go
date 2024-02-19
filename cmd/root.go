3d92ce1547b865892b2f32796d959e8a49e5d26a
João Victor da Silva <jv782063@gmail.com>
2024-02-19 17:36:56 -0300

    init cobra

---
+++ cmd/root.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+
+
+var rootCmd = &cobra.Command{
+	Use:   "learn-cli-go",
+	Short: "CLI application for manipulating authors and books",
+	Long: ``,
+
+  Run: func(cmd *cobra.Command, args []string) {
+    cmd.Help()
+  },
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("help", "h", false, "learn-cli-go help commands")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/joaovds/learn-cli-go/cmd"
+
+func main() {
+	cmd.Execute()
+}
